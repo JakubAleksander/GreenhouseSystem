1ad3ba6d7a3479cc2813345d5a9cf722529ecf0e
JakubAleksander <qbasawicki@wp.pl>
2020-04-29 01:14:31 +0200

    Add mqtt functions

--- GreenhouseSystem/src/mainwindow.h
+++ GreenhouseSystem/src/mainwindow.h
@@ -2,10 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMqttClient>
 
 namespace Ui {
 class MainWindow;
 }
+
+struct Current_parameter{
+    quint8 id;
+    int temperature;
+    int insolation;
+    int humidity;
+    bool actual_pump_status;
+    bool actual_light_status;
+    bool actual_fan_status;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +26,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    //Mqtt slots
+    void slotErrorChanged(const QMqttClient::ClientError e);
+    void slotStateChanged();
+    void slotDisconnected();
+    void slotConnected();
+
 private:
     Ui::MainWindow *ui;
+
+    QMqttClient *m_mqttClient;
 };
 
 #endif // MAINWINDOW_H
