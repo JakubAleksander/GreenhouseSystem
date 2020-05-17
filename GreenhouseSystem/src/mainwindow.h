310faef7703ec390d2ae2829d79432a693170572
JakubAleksander <qbasawicki@wp.pl>
2020-05-17 02:04:28 +0200

    Add Messenger class

--- GreenhouseSystem/src/mainwindow.h
+++ GreenhouseSystem/src/mainwindow.h
@@ -7,6 +7,7 @@
 #include <QMap>
 #include "digitalclock.h"
 #include "sectionsmanager.h"
+#include "messenger.h"
 
 namespace Ui {
 class MainWindow;
@@ -21,25 +22,15 @@ public:
     ~MainWindow();
 
 private slots:
-    //Mqtt slots
-    void slotErrorChanged(const QMqttClient::ClientError e);
-    void slotStateChanged();
-    void slotDisconnected();
-    void slotConnected();
-
     void on_btn_addSection_clicked();
 
     void on_btn_quit_clicked();
 
 signals:
-    void signalNewParamsFromGreenhouse(Current_parameters parameters);
 
 private:
     Ui::MainWindow *ui;
-
-    QMqttClient *m_mqttClient;
-    QMap<quint8, Section*>sections;
-
+    Messenger *messenger;
 };
 
 #endif // MAINWINDOW_H
